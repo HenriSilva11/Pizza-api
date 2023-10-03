3b130d5cceaa3cdf3ca215f6baa23437f00e9027
henri <pequenohenri72@gmail.com>
2023-10-03 18:26:00 -0300

    função de inserir pronta

--- src/controller/pizzariaControler.js
+++ src/controller/pizzariaControler.js
@@ -1,10 +1,10 @@
-import { Consultar } from "../repository/repositoryPizza.js";
+import { Consultar , Adicionarpizza} from "../repository/repositoryPizza.js";
 
 import { Router } from "express";
 
 let endpoints = Router()
 
-endpoints.post('/consultar', async (req, resp) => {
+endpoints.get('/consultar', async (req, resp) => {
     try{
         let pizza = req.body
 
@@ -16,4 +16,16 @@ endpoints.post('/consultar', async (req, resp) => {
         resp.status(500).send({err: 'ocorreu um erro!'})
     }
     
+})
+
+endpoints.post('/adicionar', async (req, resp) => {
+    try{
+        let pizza = req.body
+        let resposta = await Adicionarpizza(pizza)
+        resp.send(resposta)
+    }
+
+    catch(err){
+        resp.status(500).send({err: 'ocorreu um erro'})
+    }
 })
--- src/repository/repositoryPizza.js
+++ src/repository/repositoryPizza.js
@@ -9,10 +9,29 @@ export async function Consultar(){
        DS_INGREDIENTES AS INGREDIENTES,
        DS_FOTO AS FOTO
        FROM TB_PIZZA;
-    
     `
 
     let [dados] = await conn.query(sql)
     return dados;
 }
 
+export async function Adicionarpizza(pizza){
+    let sql = `
+            SELECT ID_PIZZA AS ID,
+            NM_PIZZA  AS NOME,
+            VL_PRECO  AS PREÇO,
+            QTD_QUANTIDADE AS QUANTIDADE,
+            DS_INGREDIENTES AS INGREDIENTES,
+            DS_FOTO AS FOTO
+            FROM TB_PIZZA;
+    `  
+    let [info] = await conn.query(sql, [pizza.nome,
+                                        pizza.preco,
+                                        pizza.quantidade,
+                                        pizza.ingrediantes,
+                                        pizza.foto])
+
+pizza.id = info.InsertId
+return pizza
+
+}
