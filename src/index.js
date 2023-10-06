e01152bbefbf91628a013419b82d7bfea3290de3
igao <nascimentoigor1311@gmail.com>
2023-10-05 21:27:17 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import endpoints from './controller/pizzariaControler.js';
+import cors from 'cors';
 
 const server = express();
 
 server.use(express.json());
+server.use(cors())
 server.use(endpoints);
 
 server.listen(process.env.PORT, () => console.log("Api subiu na porta " + process.env.PORT));
